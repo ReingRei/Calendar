bd7106cec74be2a8b4c42df917efe19e29524e39
Reinaldo Filho <reinaldofilho76@gmail.com>
2024-01-23 01:13:57 -0300

    feat: criacao de estrutura de arquivos do frontend

--- calendar-backend/src/app.module.ts
+++ calendar-backend/src/app.module.ts
@@ -4,6 +4,9 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entitys/user.entity';
 import { EventEntity } from './entitys/event.entity';
+import { AuthModule } from './modules/auth/auth.module';
+import { EventModule } from './modules/event/event.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
@@ -14,7 +17,10 @@ import { EventEntity } from './entitys/event.entity';
       logging: true,
       entities: ["dist/**/*.entity{.ts,.js}"],
     }),
-    TypeOrmModule.forFeature([UserEntity, EventEntity])
+    TypeOrmModule.forFeature([UserEntity, EventEntity]),
+    AuthModule,
+    EventModule,
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
